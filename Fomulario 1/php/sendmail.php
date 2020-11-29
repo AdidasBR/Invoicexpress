7bb2d0548ff77120c0b80fdc587942c3f75346f0
Rodrigo <rrossellini@hotmail.com>
2020-11-29 15:48:44 +0000

    Ficheiros finais do desafio

---
+++ Fomulario 1/php/sendmail.php
@@ -0,0 +1,30 @@
+<?php
+
+
+	$para = "rrossellini@hotmail.com";
+	$assunto = "Nova subscrição Newsletter";
+
+
+    $nome = utf8_decode ($_POST['nome']);
+    $email = utf8_decode ($_POST['email']);
+    $profissao = utf8_decode ($_POST['profissao']);
+    
+    // Monta o corpo da mensagem com os campos
+    $corpo .= "Informacoes utilizador " . "\r\n" . "\r\n";
+    $corpo .= "Nome: $nome "  . "\r\n";
+    $corpo .= "Email: $email " . "\r\n";
+    $corpo .= "Profissao: $profissao " . "\r\n";
+
+
+      $headers  = 'MIME-Version: 1.0' . "\r\n";
+      $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+      $headers .= 'From: $nome <$email>';
+
+
+    
+
+    mail($para, '=?utf-8?B?'.base64_encode($assunto).'?=', $corpo);
+
+    header('Location: ../thanks.html');
+
+?>
